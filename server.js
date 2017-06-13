58ce4634f1f33f238a11b22b8f919bacd29abefc
Tobiasz12 <tobiasz63@op.pl>
2017-06-13 18:18:33 +0200

    task 5.5

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 var express = require('express');
 var app = express();
-app.use(express.static('assets'));
 
-app.get('/userform', function (req, res) {
-    const response = {
-       first_name: req.query.first_name,
-       last_name: req.query.last_name
-   };
-   res.sendFile('/index.html');
-   res.end(JSON.stringify(response));
+app.use('/store', function(req, res, next){
+    console.log('Jestem pośrednikiem przy żądaniu do /store');
+    next();
+});
 
+app.get('/', function (req, res) {
+    res.send('Hellow world');
+});
+
+app.get('/store', function (req, res) {
+    res.send('To jast sklep');
 });
 
 app.listen(3000);
+app.use(function (req, res, next) {
+    res.status(404).send('Wybacz, nie mogliśmy odnaleźć tego, czego żądasz')
+});
